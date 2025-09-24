26ec4c65be63c79b8f8d2a01767d0e682785e901
prince-dataslush <prince.u@dataslush.com>
2025-09-24 14:56:01 +0530

    adding the artifacte permission and cloud run

--- terraform-gcp/main.tf
+++ terraform-gcp/main.tf
@@ -4,9 +4,23 @@ resource "google_service_account" "service_account" {
   display_name = var.service_account_name
 }
 
+# Allow a specific user to impersonate the service account
 resource "google_service_account_iam_member" "impersonation_binding" {
-  count                  = var.impersonation_user_email != "" ? 1 : 0
-  service_account_id     = google_service_account.service_account.name
-  role                   = "roles/iam.serviceAccountTokenCreator"
-  member                 = "user:${var.impersonation_user_email}"
+  count              = var.impersonation_user_email != "" ? 1 : 0
+  service_account_id = google_service_account.service_account.name
+  role               = "roles/iam.serviceAccountTokenCreator"
+  member             = "user:${var.impersonation_user_email}"
+}
+
+# Grant project-level roles to the service account
+resource "google_project_iam_member" "service_account_roles" {
+  for_each = toset([
+    "roles/artifactregistry.admin",
+    "roles/iam.serviceAccountUser",
+    "roles/run.admin"
+  ])
+
+  project = var.project_id
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.service_account.email}"
 }
